7927b33b4e7b255fb5f1afd7c4dceb7133a95465
Jerson Gomez <jersongomez2610@hotmail.com>
2024-09-04 17:37:04 -0500

    si

--- MariShop.DAL/Repositories/GenericRepository.cs
+++ MariShop.DAL/Repositories/GenericRepository.cs
@@ -13,6 +13,36 @@ namespace MariShop.DAL.Repositories
 {
     public class GenericRepository<TModel> :IGenericRepository<TModel> where TModel : class
     {
-        private
+        private readonly YourDbContextName _dbContextName;
+
+        public GenericRepository(YourDbContextName dbContextName)
+        {
+            _dbContextName = dbContextName;
+        }
+
+        public Task<TModel> Obtener(Expression<Func<TModel, bool>> filtro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TModel> Crear(TModel modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Editar(TModel modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Eliminar(TModel modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<TModel>> Consultar(Expression<Func<TModel, bool>> filtro = null)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
